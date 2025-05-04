3745fa3d48aebfa8ce6eb9ecd04db280eeeda03d
Paulo Braz Figueiredo <paulo.braz.figueiredo@gmail.com>
2025-05-04 01:12:25 +0100

    Migrations

--- src/PBF.WorkNotes.Gateways.SQLiteMigrator/Migrations/Migration_2025040501.cs
+++ src/PBF.WorkNotes.Gateways.SQLiteMigrator/Migrations/Migration_2025040501.cs
@@ -9,8 +9,6 @@ public class Migration_2025040501 : Migration
             .WithColumn("Id").AsGuid().PrimaryKey()
             .WithColumn("Name").AsString()
             .WithColumn("IsDefault").AsBoolean();
-        Insert.IntoTable("ToDoState").Row(new { Id = Guid.NewGuid(), Name = "Active", IsDefault = true});
-        Insert.IntoTable("ToDoState").Row(new { Id = Guid.NewGuid(), Name = "Done", IsDefault = false });
     }
 
     public override void Down()
---
+++ src/PBF.WorkNotes.Gateways.SQLiteMigrator/Migrations/Migration_2025040502.cs
@@ -0,0 +1,16 @@
+﻿namespace PBF.WorkNotes.Gateways.SQLiteMigrator.Migrations;
+
+[Migration(2025040502, @"Add ""ToDoState"" data.")]
+public class Migration_2025040502 : Migration
+{
+    public override void Up()
+    {
+        Insert.IntoTable("ToDoState").Row(new { Id = Guid.NewGuid(), Name = "Active", IsDefault = true});
+        Insert.IntoTable("ToDoState").Row(new { Id = Guid.NewGuid(), Name = "Done", IsDefault = false });
+    }
+
+    public override void Down()
+    {
+        Delete.FromTable("ToDoState");
+    }
+}
